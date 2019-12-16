de319c2bedf9e269e7292a8b3a95e6cbad3e7c5e
orpere <orlando.capoeiraraiz@gmail.com>
2019-12-16 16:36:10 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  version = "~> 2.38"
+  region  = "${var.region}"
+}
+resource "aws_route53_zone" "primary" {
+  name = "example.xt"
+}
+resource "aws_route53_record" "www-dev" {
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+  name    = "www"
+  type    = "CNAME"
+  ttl     = "5"
+
+  weighted_routing_policy {
+    weight = 10
+  }
+
+  set_identifier = "dev"
+  records        = ["dev.example.xt"]
+}
+
+resource "aws_route53_record" "www-live" {
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+  name    = "www"
+  type    = "CNAME"
+  ttl     = "5"
+
+  weighted_routing_policy {
+    weight = 90
+  }
+
+  set_identifier = "live"
+  records        = ["live.example.xt"]
+}
